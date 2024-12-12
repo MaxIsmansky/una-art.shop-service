630e070ce24cb20c4465cfec231eff8beaa425e9
ismansky <maxim.ismanskiy@bennu.cloud>
2024-12-13 00:55:31 +0300

    first commit

---
+++ src/main/java/com/rapidsystems/shop_service/ShopServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rapidsystems.shop_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopServiceApplication.class, args);
+	}
+
+}
